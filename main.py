981f2919e857f31be86338a07d3e4e9c2ee817e4
Yong <yong.park123456@gmail.com>
2023-02-02 17:46:31 -0600

    cambios realizados aun falta probar

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 #convertir el csv a json
 csvFilePath = r'winemag-data.csv'
 jsonFilePath = r'winemag-data.json'
-csv_to_json(csvFilePath, jsonFilePath)
+# csv_to_json(csvFilePath, jsonFilePath)
 
 
 client = MongoClient("mongodb+srv://lab03BD:AleOscarYong01@laboratorio03uvg.7rhri6m.mongodb.net/?retryWrites=true&w=majority")
@@ -14,8 +14,15 @@ winemag = db.winemag
 
 with open(jsonFilePath, 'r') as json_file:
     data = json.load(json_file)
+    counter = 0
     for x in data:
-        winemag.insert_one(x)
+        lista = []
+        counter = 0
+        while counter != 10000:
+            counter += 1
+            lista.append(x)
+        winemag.insert_many(lista)
+        
 
 
 
